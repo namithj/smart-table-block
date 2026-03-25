ce65ccc61afadacd5bbb66333cc331d7b3b7b468
Namith Jawahar <namith.jawahar@gmail.com>
2026-03-26 00:17:04 +0530

    Add Border Options
    Add Border Options

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'aceff7d8a45a99d1c8bd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '22d5499cd2d5755498b0');
